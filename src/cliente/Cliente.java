0f4eebac6b6f892b6bafebb6b49966dc539c58f8
ZecaVargasNolli <JvargasNolli@yahoo.com.br>
2020-09-28 20:12:37 -0300

    adicionar

---
+++ src/cliente/Cliente.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cliente;
+
+import View.IpServidor;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author José Vargas Nolli
+ */
+public class Cliente extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+        
+        IpServidor root = new IpServidor();
+        
+       root.setVisible(true);
+       root.show();
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
